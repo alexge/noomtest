2e1a83ff36576376580c9d7bf36232cf8ee5bf44
alex ge <>
2021-09-09 13:22:17 +0100

    use built in validation

--- NoomTest/NoomTest/Networking/AFConsumer.swift
+++ NoomTest/NoomTest/Networking/AFConsumer.swift
@@ -24,23 +24,23 @@ enum SearchError: Error {
 class AFConsumer: SearchRequestPerformerProtocol {
     func search(_ searchTerm: String, completion: @escaping (SearchResult) -> Void) {
         let request = AF.request("https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search", method: .get, parameters: ["kv": searchTerm])
-        request.response { response in
-            switch response.result {
-            case .success(let data):
-                guard let status = response.response?.statusCode, let data = data else {
-                    completion(SearchResult(searchTerm: searchTerm, result: .failure(.any)))
-                    return
-                }
-                
-                if 200..<300 ~= status {
+        request.validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .response { response in
+                switch response.result {
+                case .success(let data):
+                    guard let data = data else {
+                        completion(SearchResult(searchTerm: searchTerm, result: .failure(.any)))
+                        return
+                    }
                     do {
                         let results = try JSONDecoder().decode([Food].self, from: data)
                         completion(SearchResult(searchTerm: searchTerm, result: .success(results)))
                     } catch {
                         completion(SearchResult(searchTerm: searchTerm, result: .failure(.decode)))
                     }
-                } else {
-                    if status == 400, let message = try? JSONDecoder().decode(String.self, from: data) {
+                case .failure:
+                    if let data = response.data, let message = try? JSONDecoder().decode(String.self, from: data) {
                         if message.contains("must be at least three characters") {
                             completion(SearchResult(searchTerm: searchTerm, result: .failure(.tooShort)))
                         }
@@ -48,9 +48,6 @@ class AFConsumer: SearchRequestPerformerProtocol {
                         completion(SearchResult(searchTerm: searchTerm, result: .failure(.any)))
                     }
                 }
-            case .failure:
-                completion(SearchResult(searchTerm: searchTerm, result: .failure(.any)))
             }
-        }
     }
 }
