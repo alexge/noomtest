d3161f243bb7f1db762fc6527b1a2a69a72513ef
alex ge <>
2021-09-09 15:05:42 +0100

    calories header view

---
+++ NoomTest/NoomTest/Views/CaloriesHeader.swift
@@ -0,0 +1,60 @@
+//
+//  CaloriesHeader.swift
+//  NoomTest
+//
+//  Created by Alexander Ge on 09/09/2021.
+//
+
+import UIKit
+
+class CaloriesHeader: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = NSLocalizedString("Calories in meal:", comment: "")
+        return label
+    }()
+    
+    private let caloriesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "0"
+        return label
+    }()
+    
+    private let contentStack: UIStackView = {
+        let sv = UIStackView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.alignment = .leading
+        sv.spacing = 8
+        return sv
+    }()
+    
+    private var calories: Double = 0 {
+        didSet {
+            caloriesLabel.text = String(calories)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(contentStack)
+        contentStack.addArrangedSubview(titleLabel)
+        contentStack.addArrangedSubview(caloriesLabel)
+        NSLayoutConstraint.activate([
+            contentStack.centerYAnchor.constraint(equalTo: centerYAnchor),
+            contentStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            contentStack.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -16)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func add(_ calories: Double) {
+        self.calories += calories
+    }
+}
